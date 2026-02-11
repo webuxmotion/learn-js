8d6951893bf1cfc136a4ebead5ceaa4601096cef
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2026-02-11 20:31:58 +0200

    fix selector

--- dom-attributes-and-properties/app.js
+++ dom-attributes-and-properties/app.js
@@ -1,7 +1,7 @@
 console.log("app.js running");
 console.log("==================");
 
-const externalLinks = document.querySelectorAll('a[href*="://"]a[href!^="http://internal.com"]');
+const externalLinks = document.querySelectorAll('a[href*="://"]:not([href^="http://internal.com"])');
 
 for (const link of externalLinks) {
     link.style.color = 'orange';
